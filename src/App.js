abf5eb52f52fef3d603c8ef228b9e0b8769f6048
Alexandre <alexandre.morel@edu.devinci.fr>
2022-11-10 19:09:22 +0100

    TP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 import ListContainer from "./components/ListContainer";
 import Cart from "./views/Tp1V2/Cart/Cart";
 import ProductList from "./views/Tp1V2/ProductList";
+import UserForm from "./views/Tp2/Form";
+import CreateCard from "./views/TP3/CreateCard";
+import CardList from "./views/TP3/CardList";
+import ReadCard from "./views/TP3/ReadCard";
 
 function App() {
   const [displayForm, setDisplayForm] = useState(true);
@@ -9,6 +13,10 @@ function App() {
     <>
       <ProductList />
       <Cart />
+      {/* <button onClick={() => setDisplayForm(!displayForm)}>Create a User</button>
+      {displayForm && <UserForm />} */}
+      <button onClick={() => setDisplayForm(!displayForm)}>Cards Manager</button>
+      {displayForm && <CardList />}
     </>
 
   );
